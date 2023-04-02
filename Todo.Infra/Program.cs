2ddd95875cba4022730cf11f158d6dec1a95835f
Laura Fumagalli <86015358+fumagallilaura@users.noreply.github.com>
2023-04-02 19:09:53 -0300

    chore: add architecture and run app by docker

---
+++ Todo.Infra/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+
+namespace Todo.Infra
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Applying migrations");
+            IWebHost webHost = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .Build();
+            Console.WriteLine("Done");
+        }
+    }
+}
