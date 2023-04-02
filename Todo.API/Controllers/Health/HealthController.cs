ad4c28057b1550ee2ed711962c7295609fe36c1a
Laura Fumagalli <86015358+fumagallilaura@users.noreply.github.com>
2023-04-02 16:33:52 -0300

    feat: add health check contrller

---
+++ Todo.API/Controllers/Health/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.API.Controllers.HealthCheck
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return Ok();
+        }
+    }
+}
